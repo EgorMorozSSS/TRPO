6ff017bdaf9a5dca9f01d6394a0f4c09d1748f6d
EgorMoroz <morozil06@mail.ru>
2024-10-22 13:37:10 +0300

    create url for main page

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from django.db.models import Count
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.http import JsonResponse
+import random
+from django.core.paginator import Paginator
+
+def main_page(request):
+    return render(request, 'page/main_page.html')
 
 # Create your views here.
