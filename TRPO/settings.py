222efa2d5dfb228aa7b9e7a224d762f9d0f7fb2a
EgorMoroz <morozil06@mail.ru>
2024-10-28 11:24:54 +0300

    update main page view, add new folders for scss and images, add scss styles for main page

--- TRPO/settings.py
+++ TRPO/settings.py
@@ -117,8 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Измените на свой путь, если необходимо
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
